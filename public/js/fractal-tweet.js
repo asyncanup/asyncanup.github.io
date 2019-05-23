d8bee0ecd4868203ee65c98c9035264345909382
Anup Bishnoi <pixelsallover@gmail.com>
2019-05-23 11:31:40 -0700

    Update fractal-tweet.js

--- public/js/fractal-tweet.js
+++ public/js/fractal-tweet.js
@@ -1,8 +1,12 @@
 v=document.createElement('canvas')
-v.width="400px"
-v.height="400px"
+v.width=500
+v.height=500
 v.style.backgroundColor='#f4f5f2'
-v.onclick=()=>{c.clearRect(0,0,v.width,v.height);F()}
+v.onclick=()=>{
+  c.clearRect(0,0,v.width,v.height)
+  clearTimeout(T)
+  F()
+}
 document.querySelector('#canvas-container').appendChild(v);
 
 F=()=>{
@@ -10,11 +14,11 @@ c=v.getContext('2d')
 c.lineWidth=.03
 C=[1,-.5,-.5]
 S=[0,.87,-.87]
-P=[[X=300,Y=300],[X,Y],[X,Y]]
+P=[[X=250,Y=250],[X,Y],[X,Y]]
 R=()=>{d=parseInt(Math.random()*3)
 P=P.map (([x,y],i)=>(c.moveTo(x,y),c.lineTo(X=x+3*C[I=(d+i)%3],Y=y+3*S[I]),[X,Y]))
 c.stroke()
-setTimeout(R,17)
+T=setTimeout(R,17)
 }
 R()}
 
