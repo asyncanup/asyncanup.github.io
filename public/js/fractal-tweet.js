cb85beb3554f22255186a9422186ded5970d3a66
Anup Bishnoi <pixelsallover@gmail.com>
2019-05-23 11:43:28 -0700

    Update fractal-tweet.js

--- public/js/fractal-tweet.js
+++ public/js/fractal-tweet.js
@@ -4,6 +4,8 @@ v.height=500
 v.style.backgroundColor='#f4f5f2'
 v.onclick=()=>{
   c.clearRect(0,0,v.width,v.height)
+  c.closePath()
+  c.beginPath()
   clearTimeout(T)
   F()
 }
@@ -16,7 +18,7 @@ C=[1,-.5,-.5]
 S=[0,.87,-.87]
 P=[[X=250,Y=250],[X,Y],[X,Y]]
 R=()=>{d=parseInt(Math.random()*3)
-P=P.map (([x,y],i)=>(c.moveTo(x,y),c.lineTo(X=x+3*C[I=(d+i)%3],Y=y+3*S[I]),[X,Y]))
+P=P.map(([x,y],i)=>(c.moveTo(x,y),c.lineTo(X=x+3*C[I=(d+i)%3],Y=y+3*S[I]),[X,Y]))
 c.stroke()
 T=setTimeout(R,17)
 }
